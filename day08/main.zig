aca1552fdff36f849df4870ab56901c1ee5987dd
Andrew Rogers <andrurogerz@gmail.com>
2023-12-08 11:36:35 -0800

    day 08 part 02 solution

--- day08/main.zig
+++ day08/main.zig
@@ -30,6 +30,70 @@ fn part_1(comptime input: []const u8) !usize {
     return traversal_count;
 }
 
+fn part_2(comptime input: []const u8) !usize {
+    var line_iter = std.mem.tokenizeSequence(u8, input, "\n");
+    const directions_line = line_iter.next() orelse return error.Unexpected;
+
+    var graph = Graph{};
+    while (line_iter.next()) |line| {
+        try graph.addNode(line);
+    }
+
+    // Find all the possible start nodes.
+    var node_ids: [16]u16 = undefined;
+    var node_count: usize = 0;
+    for (Graph.START_NODE_ID..Graph.MAX_NODE_ID) |id| {
+        if (graph.nodes[id] == null) continue;
+        if (Graph.Node.str(@intCast(id))[2] == 'A') {
+            std.debug.assert(node_count < node_ids.len);
+            node_ids[node_count] = @intCast(id);
+            node_count += 1;
+        }
+    }
+
+    std.debug.assert(node_count != 0);
+
+    var traversal_counts: [node_ids.len]usize = [_]usize{0} ** node_ids.len;
+    for (0..node_count) |idx| {
+        const start_id = node_ids[idx];
+        var next_id: u16 = start_id;
+        while (traversal_counts[idx] < std.math.maxInt(u24)) {
+            if (Graph.Node.str(next_id)[2] == 'Z') {
+                break;
+            }
+
+            const dir_ch = directions_line[traversal_counts[idx] % directions_line.len];
+            const node = graph.nodes[next_id] orelse return error.Unexpected;
+            next_id = switch (dir_ch) {
+                'L' => node.left,
+                'R' => node.right,
+                else => unreachable,
+            };
+
+            traversal_counts[idx] += 1;
+        }
+    }
+
+    var result = traversal_counts[0];
+    for (traversal_counts[1..node_count]) |count| {
+        result = lcm(result, count);
+    }
+
+    return result;
+}
+
+/// Calcualte least common multiple of two integers.
+fn lcm(a: usize, b: usize) usize {
+    if (a > b) return (a / gcd(a, b)) * b;
+    return (b / gcd(a, b)) * a;
+}
+
+/// Calcualte greatest common denominator of two integers.
+fn gcd(a: usize, b: usize) usize {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
 const Graph = struct {
     const Self = @This();
 
@@ -95,36 +159,56 @@ const Graph = struct {
 
 pub fn main() !void {
     const input = @embedFile("./input.txt");
-    const result = try part_1(input);
-    std.debug.print("part 1 result: {}\n", .{result});
+    {
+        const result = try part_1(input);
+        std.debug.print("part 1 result: {}\n", .{result});
+    }
+    {
+        const result = try part_2(input);
+        std.debug.print("part 2 result: {}\n", .{result});
+    }
 }
 
 const testing = std.testing;
 
-const EXAMPLE_INPUT_1 =
-    \\RL
-    \\
-    \\AAA = (BBB, CCC)
-    \\BBB = (DDD, EEE)
-    \\CCC = (ZZZ, GGG)
-    \\DDD = (DDD, DDD)
-    \\EEE = (EEE, EEE)
-    \\GGG = (GGG, GGG)
-    \\ZZZ = (ZZZ, ZZZ)
-;
-
-const EXAMPLE_INPUT_2 =
-    \\LLR
-    \\
-    \\AAA = (BBB, BBB)
-    \\BBB = (AAA, ZZZ)
-    \\ZZZ = (ZZZ, ZZZ)
-;
-
 test "part 1 example input 1" {
-    try testing.expectEqual(part_1(EXAMPLE_INPUT_1), 2);
+    const EXAMPLE_INPUT =
+        \\RL
+        \\
+        \\AAA = (BBB, CCC)
+        \\BBB = (DDD, EEE)
+        \\CCC = (ZZZ, GGG)
+        \\DDD = (DDD, DDD)
+        \\EEE = (EEE, EEE)
+        \\GGG = (GGG, GGG)
+        \\ZZZ = (ZZZ, ZZZ)
+    ;
+    try testing.expectEqual(part_1(EXAMPLE_INPUT), 2);
 }
 
 test "part 1 example input 2" {
-    try testing.expectEqual(part_1(EXAMPLE_INPUT_2), 6);
+    const EXAMPLE_INPUT =
+        \\LLR
+        \\
+        \\AAA = (BBB, BBB)
+        \\BBB = (AAA, ZZZ)
+        \\ZZZ = (ZZZ, ZZZ)
+    ;
+    try testing.expectEqual(part_1(EXAMPLE_INPUT), 6);
+}
+
+test "part 2 example input" {
+    const EXAMPLE_INPUT =
+        \\LR
+        \\
+        \\JJA = (JJB, XXX)
+        \\JJB = (XXX, JJZ)
+        \\JJZ = (JJB, XXX)
+        \\KKA = (KKB, XXX)
+        \\KKB = (KKC, KKC)
+        \\KKC = (KKZ, KKZ)
+        \\KKZ = (KKB, KKB)
+        \\XXX = (XXX, XXX)
+    ;
+    try testing.expectEqual(part_2(EXAMPLE_INPUT), 6);
 }
